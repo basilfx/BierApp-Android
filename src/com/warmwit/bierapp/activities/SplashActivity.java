0dbf524ac27f76757f1d5583b5fdeec53746db9d
Bas Stottelaar <basstottelaar@gmail.com>
2013-04-11 02:47:08 +0200

    Improved splash activity error handling.

--- src/com/warmwit/bierapp/activities/SplashActivity.java
+++ src/com/warmwit/bierapp/activities/SplashActivity.java
@@ -1,8 +1,13 @@
 package com.warmwit.bierapp.activities;
 
+import static com.google.common.base.Preconditions.checkNotNull;
+
 import java.io.IOException;
 import java.sql.SQLException;
 
+import android.app.AlertDialog;
+import android.app.AlertDialog.Builder;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -18,58 +23,160 @@ import com.warmwit.bierapp.data.ApiConnector;
 import com.warmwit.bierapp.data.RemoteClient;
 import com.warmwit.bierapp.database.DatabaseHelper;
 
+/**
+ * Splash screen activity. Loads data from server, then advances to 
+ * HomeActivity.
+ * 
+ * @author Bas Stottelaar
+ */
 public class SplashActivity extends OrmLiteBaseActivity<DatabaseHelper> {
 	
+	/**
+	 * @const Logging tag
+	 */
+	public static final String LOG_TAG = "SplashActivity";
+	
+	/**
+	 * @const Timeout before advancing to next screen in ms
+	 */
+	public static final int SPLASH_TIMEOUT = 500;
+	
+	/**
+	 * @var Reference to data client
+	 */
 	private RemoteClient remoteClient;
+	
+	/**
+	 * @var Reference to API connector
+	 */
 	private ApiConnector apiConnector;
 	
+	/**
+	 * {@inheritDoc}
+	 */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         
-        this.remoteClient = ((BierAppApplication) this.getApplication()).getRemoteClient();
-		this.apiConnector = new ApiConnector(remoteClient, this.getHelper());
+        // Logging
+        Log.i(LOG_TAG, "Started application");
         
-        // Make splash screen full size
+        // Configure activity
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         this.getWindow().setFlags(LayoutParams.FLAG_FULLSCREEN, LayoutParams.FLAG_FULLSCREEN);
-        
-        // Set content
         this.setContentView(R.layout.activity_splash);
+        
+        // Create API instance
+        this.remoteClient = ((BierAppApplication) this.getApplication()).getRemoteClient();
+		this.apiConnector = new ApiConnector(remoteClient, this.getHelper());
         
         // Load data and advance to next screen
         new LoadDataTask().execute();
     }
     
-    private class LoadDataTask extends AsyncTask<Void, Void, Void> {
+    /**
+     * Sync data task.
+     */
+    private class LoadDataTask extends AsyncTask<Void, Void, Integer> {
 		@Override
-		protected Void doInBackground(Void... params) {
-			// Warmup cache
+		protected Integer doInBackground(Void... params) {
 			try {
+				// Products
+				Log.d(LOG_TAG, "Loading products");
 				SplashActivity.this.apiConnector.loadProducts();
+				
+				// Users
+				Log.d(LOG_TAG, "Loading users");
 				SplashActivity.this.apiConnector.loadUsers();
+				
+				// Transactions
+				Log.d(LOG_TAG, "Loading transactions");
 				SplashActivity.this.apiConnector.loadTransactions();
+				
+				// Users info
+				Log.d(LOG_TAG, "Loading users info");
 				SplashActivity.this.apiConnector.loadUserInfo();
 			} catch (IOException e) {
-				Log.e(this.getClass().getName(), e.getMessage());
+				return this.handleException(e, 1);
 			} catch (SQLException e) {
-				Log.e(this.getClass().getName(), e.getMessage());
+				return this.handleException(e, 2);
 			}
 	        
-	        return null;
+			// Done
+	        return 0;
 		}
 
+		/**
+		 * Runs the appropriate action for a given result. The result is either
+		 * zero to indicate success or another integer to indicate failure.
+		 * 
+		 * @param result Integer result of the task
+		 */
 		@Override
-		protected void onPostExecute(Void result) {
-			super.onPostExecute(result);
+		protected void onPostExecute(Integer result) {
+			checkNotNull(result);
 			
-			// Advance to next screen, with little delay
-			new Handler().postDelayed(new Runnable(){
-				public void run() {
-					Intent intent = new Intent(SplashActivity.this, HomeActivity.class);
-					startActivity(intent);             
+			switch (result) {
+				case 0:
+					// Advance to next screen, with little delay
+					new Handler().postDelayed(new Runnable(){
+						public void run() {
+							Log.d(LOG_TAG, "Loading HomeActivity.");
+							
+							Intent intent = new Intent(SplashActivity.this, HomeActivity.class);
+							startActivity(intent);             
+						}
+					}, SplashActivity.SPLASH_TIMEOUT);
+					
+					break;
+				case 1:
+					this.showDialog("Internetverbinding", "Kan de server niet benaderen. Controleer of er een actieve internetverbinding is.");
+					break;
+				case 2:
+					this.showDialog("Applicatiefout", "Interne cache is corrupt. Probeer de applicatiegegevens te wissen en probeer het opnieuw.");
+					break;
+				default:
+					// Should not land here
+					throw new IllegalStateException();
+			}
+		}
+		
+		/**
+		 * Internal helper to log exception and return a return value.
+		 * 
+		 * @param exception Caugh exception
+		 * @param returnValue Integer return value
+		 * @return given returnValue
+		 */
+		private int handleException(Exception exception, int returnValue) {
+			Log.e(LOG_TAG, "Caught exception: " + exception.getMessage());
+			return returnValue;
+		}
+		
+		/**
+		 * Internal helper to display a dialog with an OK button. After clicking,
+		 * the activity will finish, thus quit.
+		 * 
+		 * @param title Dialog title
+		 * @param message Dialog message
+		 */
+		private void showDialog(String title, String message) {
+			AlertDialog.Builder builder = new AlertDialog.Builder(SplashActivity.this);
+			
+			// Configure alert dialog
+			builder.setTitle(title);
+			builder.setMessage(message);
+			builder.setCancelable(false);
+			builder.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+				@Override
+				public void onClick(DialogInterface dialog, int which) {
+					// Closes application
+					SplashActivity.this.finish();
 				}
-			}, 1);
+			});
+			
+			// Show dialog
+			builder.show();
 		}
     }
 }
