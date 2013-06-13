292311644e9bf1617e53ab99a8a92c9e505a78f8
Bas Stottelaar <basstottelaar@gmail.com>
2013-06-13 13:17:21 +0200

    Fix imports

--- src/com/warmwit/bierapp/data/models/Transaction.java
+++ src/com/warmwit/bierapp/data/models/Transaction.java
@@ -2,9 +2,7 @@ package com.warmwit.bierapp.data.models;
 
 import java.util.Date;
 
-import com.j256.ormlite.dao.ForeignCollection;
 import com.j256.ormlite.field.DatabaseField;
-import com.j256.ormlite.field.ForeignCollectionField;
 import com.j256.ormlite.table.DatabaseTable;
 
 @DatabaseTable(tableName = "transaction")
--- src/com/warmwit/bierapp/database/ProductHelper.java
+++ src/com/warmwit/bierapp/database/ProductHelper.java
@@ -12,7 +12,6 @@ import com.j256.ormlite.dao.CloseableIterator;
 import com.j256.ormlite.stmt.QueryBuilder;
 import com.j256.ormlite.stmt.Where;
 import com.warmwit.bierapp.data.models.Product;
-import com.warmwit.bierapp.data.models.User;
 
 public class ProductHelper extends QueryHelper {
 	
